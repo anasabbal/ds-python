f42ae26c7a68a5a65e41d53ddf26d308bcaff027
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-10-26 15:43:07 +0100

    extends doubly linked list to 60%

--- linkedlist/part II/doubly/DoublyLinkedlist.py
+++ linkedlist/part II/doubly/DoublyLinkedlist.py
@@ -22,15 +22,98 @@ class DoublyLinkedList:
 
         return new_node
 
+    def insertAtEnd(self, data):
+        if self.head is None:
+            new_node = self.createNode(data)
+            self.head = new_node
+            return new_node
+        new_node = self.createNode(data)
+        current_node = self.head
+        # search last node with next null
+        while current_node.next is not None:
+            current_node = current_node.next
+
+        current_node.next = new_node
+        new_node.previous = current_node
+
+        return new_node
+
+    def size(self):
+        current = self.head
+        sum = 0
+        while current:
+            sum += 1
+            current = current.next
+        return sum
+
+    def findLastNode(self):
+        if self.head is None:
+            return None
+        current = self.head
+        while current.next != None:
+            current = current.next
+        return current
+
+    def searchByIndex(self, index):
+        if index > self.size() and index == 0:
+            print("Index Out of range list")
+            return
+        if index == self.size():
+            return self.findLastNode().data
+        if index == 0:
+            return self.head.data
+
+        current = self.head
+        for i in range(index - 1):
+            current = current.next
+            if(i is index):
+                return current
+
+
+    def insertAtPosition(self, index, data):
+        if self.head is None:
+            new_node = self.createNode(data)
+            self.head = new_node
+            return new_node
+
+        if index > self.size() and index == 0:
+            print("Index Out of range list")
+            return
+        
+        new_node = self.createNode(data)
+        current = self.head
+
+        for i in range(index - 2):
+            current = self.head.next
+
+        new_node.previous = current 
+        new_node.next = current.next 
+        current.next = new_node 
+        new_node.next.previous = new_node
+        
+        return new_node
+
+    
     def display(self):
         temp = self.head
         while temp is not None:
             print(temp.data, end=' ')
             temp = temp.next
+        print()
 
 if __name__ == '__main__':
     lst = DoublyLinkedList()
+    # add
     lst.insertAtBegin(3)
     lst.insertAtBegin(2)
     lst.insertAtBegin(1)
+    lst.insertAtPosition(2, 8)
+    # size
+    print(lst.size(), end="\n")
+    # display
     lst.display()
+    # find last node
+    print(lst.findLastNode().data)
+
+    print(lst.searchByIndex(2))
+
