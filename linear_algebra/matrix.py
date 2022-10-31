54df0caa0b230bd1477f95818cb320f0b102ec50
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-10-31 16:10:11 +0100

    mult matrix

--- linear_algebra/matrix.py
+++ linear_algebra/matrix.py
@@ -84,6 +84,45 @@ class Matrix(object):
 
         return new_matrix
 
+    def __scalar__(self, second):
+        new_matrix = Matrix(self.rows, self.columns)
+
+        for row in range(self.rows):
+            for col in range(self.columns):
+                new_matrix[row][col] = self[row][col] * second
+        return new_matrix
+
+    def __transpose__(self):
+        new_matrix = Matrix(self.columns, self.rows)
+
+        for row in range(self.rows):
+            for col in range(self.columns):
+                new_matrix[row][col] = self.matrix[row][col] # a(i,j) = a(j,i)
+        return new_matrix
+
+    def __mul__(self, second):
+        if isinstance(second, (int, float, complex)):
+            return self.__scalar__(second)
+        elif isinstance(second, Matrix):
+            if self.columns == second.rows:
+                new_matrix = Matrix(self.rows, second.columns)
+                transpose_matrix = self.__transpose__()
+
+                for row in range(self.rows):
+                    for row_transpose in range(transpose_matrix.rows):
+                        new_element = 0
+                        for col in range(self.columns):
+                            new_element += (self[row][col] * transpose_matrix[row_transpose][col])
+                        new_matrix[row][row_transpose] = new_element
+                return new_matrix
+            else:
+                raise Exception(
+                    "Can't multiply (%d, %d) matrix with (%d, %d) matrix" %
+                    (self.rows, self.columns, second.rows, second.columns)
+                )
+        else:
+            raise TypeError("Can't multiply a matrix by non-int of type " + type(second).__name__)
+
     def __add__(self, second):
         return self.__add_or_sub(second, "add")
 
